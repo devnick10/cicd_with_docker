58a47ac53ed5abc1f6b67b6c3a79f225e0fd56df
devnick10 <nickbhoyar683@gmail.com>
2026-02-28 14:49:12 +0530

    Add header to TODO app page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,6 +5,7 @@ import { Todos } from '../components/todos'
 const page = () => {
   return (
     <div>
+    <h1 style={{textAlign:"center",marginTop:"12px"}}>TODO APP</h1>
       <TodoInput />
       <Todos />
     </div>
